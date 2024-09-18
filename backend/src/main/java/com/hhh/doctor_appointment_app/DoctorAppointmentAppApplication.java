4862ad49353d0493c06ee6f4add3fc3083680dcb
Vu Xuan Hoang <xuanhoang949@gmail.com>
2024-09-19 00:24:02 +0700

    first commit

---
+++ backend/src/main/java/com/hhh/doctor_appointment_app/DoctorAppointmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.hhh.doctor_appointment_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorAppointmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorAppointmentAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hhh/doctor_appointment_app/DoctorAppointmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhh.doctor_appointment_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorAppointmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
