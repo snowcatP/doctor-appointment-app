9dc45b9087bf55ee0c6b33f65b0c4bb6babbf37f
hiepbui32 <hiepbui2812@gmail.com>
2024-09-22 16:51:08 +0700

    Set up structure for FE

---
+++ frontend/src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { AfterContentInit, AfterViewInit, Component, OnChanges, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent implements OnInit{
+  ngOnInit(): void {
+      
+  }
+ 
+
+}
