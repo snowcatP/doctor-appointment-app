3a2ea7117160628bf4e03fe060929a41596a857b
Vu Xuan Hoang <xuanhoang949@gmail.com>
2024-11-02 17:28:20 +0700

    Fix cors with POST request

--- backend/src/main/java/com/hhh/doctor_appointment_app/controller/AuthenticationController.java
+++ backend/src/main/java/com/hhh/doctor_appointment_app/controller/AuthenticationController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.text.ParseException;
 
 @RestController
-@CrossOrigin
+@CrossOrigin("*")
 @RequestMapping("api/auth")
 public class AuthenticationController {
     @Autowired
