16428221978809af9eeec813c8a4cc0acf8bc4f0
Vu Xuan Hoang <xuanhoang949@gmail.com>
2024-11-02 15:01:46 +0700

    Fix authen error

--- frontend/src/app/core/interceptors/authenticate.interceptor.ts
+++ frontend/src/app/core/interceptors/authenticate.interceptor.ts
@@ -1,12 +1,33 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { jwtDecode } from 'jwt-decode';
+
+
+function isTokenExpired(token: string): boolean {
+  try {
+    const { exp } = jwtDecode<{ exp: number }>(token);
+    return exp * 1000 < Date.now();
+  } catch (error) {
+    return true;
+  }
+}
 
 export const appInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
   const authToken = localStorage.getItem('token');
   if (authToken != null) {
-    const clonedRequest = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${localStorage.getItem('token')}`)
-    });
-    return next(clonedRequest);
+
+    if (!isTokenExpired(authToken)) {
+      const clonedRequest = req.clone({
+        headers: req.headers.set(
+          'Authorization',
+          `Bearer ${localStorage.getItem('token')}`
+        ),
+      });
+      return next(clonedRequest);
+    } else {
+      localStorage.removeItem('token');
+    }
   }
   return next(req);
 };
+
+
